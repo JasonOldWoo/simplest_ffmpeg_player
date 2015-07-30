f05e8ba85ba8d145f8f4eac0d8a3a4025d1e9963
JasonOldWoo <brucecen2@gmail.com>
2015-07-30 17:29:14 +0800

    1.fix some compiling error

--- simplest_ffmpeg_player/compile_gcc.sh
+++ simplest_ffmpeg_player/compile_gcc.sh
@@ -10,4 +10,4 @@
 #
 #compile
 gcc simplest_ffmpeg_player.cpp -g -o simplest_ffmpeg_player.out \
--I /usr/local/include -L /usr/local/lib -lSDL2main -lSDL2 -lavformat -lavcodec -lavutil -lswscale
+-I /usr/local/include -L /usr/local/lib -lSDL2main -lSDL2 -lavformat -lavcodec -lavutil -lswscale -lm -lpthread -lz -lswresample
